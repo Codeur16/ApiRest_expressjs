92eb497aeb8ed0aeef28d3b110a0ce00e08e65a6
Codeur16 <charlesnjiosseu@gmail.com>
2023-02-28 13:47:01 +0100

    deployment

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 
@@ -8,18 +8,21 @@ const sequelize = require('./src/db/sequelize');
 const app = express();
 
 
-const port=3001;
+const port=process.env.PORT || 3001;
 
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(favicon(__dirname + '/favicon.ico'));
 app.use(bodyParser.json());
 app
     .use(favicon(__dirname + "/favicon.ico"))
-    .use(morgan('dev'))
+    // .use(morgan('dev'))
     .use(bodyParser.json());
 //sequelize.initDB();
 // POINT DE TERMINAISON
+app.get('/',(req, res)=>{
+    res.send('Hello Hiruko');
+})
 require("./src/routes/findAllUser")(app);
 require("./src/routes/findOneUser")(app);
 require("./src/routes/createUser")(app);
